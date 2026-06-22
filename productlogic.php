e1e71f4fc49bd0122b93b92e78c543dbed6ce2d7
ghaniafahad16-rgb <ghaniafahad16@gmail.com>
2026-06-22 22:08:41 +0500

    Implement product insertion with image upload
    Added product insertion logic with file upload handling.

--- productlogic.php
+++ productlogic.php
@@ -1,3 +1,40 @@
 <?php
-$pname=$
+
+include '../Auth/connection.php';
+$name=$_REQUEST['pname'];
+$price=$_REQUEST['price'];
+$sku=$_REQUEST['sku'];
+$stock=$_REQUEST['stock'];
+$desc=$_REQUEST['desc'];
+$catid=$_REQUEST['catid'];
+
+$pic=$_FILES['pic'];
+$picname=$pic['name'];
+$tmpname=$pic['tmp_name'];
+
+$newpicname=uniqid()."_". round(microtime(true)*1000)."_".$picname;
+$folder="../images/$newpicname";
+if(move_uploaded_file($tmpname,$folder)){
+$responce=mysqli_query($connection,"Insert into products(name,quantity,price,description,sku,category,pic)
+values('{$name}'),('{$price}'),('{$stock}'),('{$desc}'),('{$sku}',('{$catid}'),('{$newpicname}'))");
+
+
+if($responce){
+    echo"
+    <script>
+    window.location.href='allproduct.php'
+    </script>
+    ";}
+    else{
+        echo"
+        <script>
+window.location.href='insertproduct.php'
+
+        </script>
+        ";
+    
+    
+    }
+    
+    }
 ?>
